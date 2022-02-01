9ae44b697906cbaa1d0d62a001ab111724bac30e
Ted Dunstone <ted@biometix.com>
2022-02-01 12:26:35 +0800

    update

--- streamlit_run.py
+++ streamlit_run.py
@@ -14,25 +14,38 @@ if img_file_buffer is None:
     st.stop()
 
 finger = Image.open(img_file_buffer)
-val= st.sidebar.slider("Image Offset", 0, 64, 1)
+offset_val= st.sidebar.slider("Image Width Adjust", -64, 64, 0, 1)
 
 fp1=finger.convert('L')
 st.sidebar.image(fp1)
 pix = numpy.array(fp1)
-val = pix.shape[1]-val
+val = pix.shape[1]-offset_val
 raw=pix.flatten()
-#for i in range(30,40):
-#  const=i*100;
-#
+
+
 rows=int(len(raw)/val)
 size=[rows,val]
 raw=raw[:val*(rows)]
 pix2=raw.reshape(size)
-
 im = Image.fromarray(numpy.uint8(pix2))
+
 st.image(im)
+#st.write("blah")
+#st.image(im2)
 with io.BytesIO() as output:
     im_format = st.selectbox("Select a image format", ["png","jpg","wsq"])
     im.save(output, format=im_format)
     contents = output.getvalue()
     st.download_button(label="download image",data=contents, file_name="fixed_image."+im_format)
+
+with st.expander("Raw Binary Data"):
+    res=numpy.array(im.copy().crop((0,0,1,rows))) #.histogram()
+    st.write("Standard Deviation")
+    st.write(numpy.std(res))
+    st.line_chart(res)
+    c1,c2=st.columns(2)
+    c1.write("Start of File")
+    c1.write([numpy.base_repr(v, base=16) for v in raw[:abs(offset_val)]])
+    c2.write("End of File")
+    c2.write([numpy.base_repr(v, base=16) for v in raw[-abs(offset_val):]])
+    
